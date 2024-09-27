c4fcd2a921ee9dfdf6ef39f472dd9719c6d347ae
mahdi <mahdimamashli1383@gmail.com>
2024-09-28 03:05:00 +0330

    test(unit): The test was updated according to the changes in the future version

--- test/unit/cli.spec.ts
+++ test/unit/cli.spec.ts
@@ -2,28 +2,54 @@ import { expect } from "chai";
 import { describe, beforeEach, it } from "mocha";
 import { Qiu } from "../../lib/Qiu";
 import path from "path";
-describe("CLI namespace", () => {
+describe("Qiu", function () {
   let qiu: Qiu;
-  beforeEach(async () => {
-    qiu = new Qiu(
-      "mariadb",
-      `-u ${process.env.DB_USERNAME} -p${process.env.DB_PASSWORD}`
-    );
-    await qiu.exec("CREATE DATABASE IF NOT EXISTS test_qiu");
-    qiu.use("test_qiu");
-  });
-  it("return error for invalid Sql", () => {
-    qiu.exec("SHOW DATABASE;").catch((err: any) => {
-      expect(err.message).to.include("You have an error in your SQL syntax");
+  beforeEach(async function () {
+    qiu = new Qiu({
+      type: "postgresql",
+      connect: "psql -U postgres -d postgres -h localhost -p 5432",
+    });
+
+    // Drop the database if it exists and create it cleanly
+    await qiu.exec("DROP DATABASE IF EXISTS test_qiu", { value: true });
+    await qiu.exec("CREATE DATABASE test_qiu", { value: true });
+
+    // Ensure the database is created before reconnecting
+    let dbExists = false;
+    while (!dbExists) {
+      const checkDb = await qiu.exec(
+        "SELECT 1 FROM pg_database WHERE datname = 'test_qiu';",
+        { value: true }
+      );
+      dbExists = checkDb!.includes("1");
+    }
+
+    // Reconnect to the new test_qiu database
+    qiu = new Qiu({
+      type: "postgresql",
+      connect: "psql -U postgres -d test_qiu -h localhost -p 5432",
     });
   });
-  it("load Sql Query With Files", async () => {
-    const createTableQuery = path.join(__dirname, "sql", "user_table.sql");
-    const tableName = "user";
-    await qiu.exec(createTableQuery);
+  afterEach(async () => {
+    qiu.close();
+  });
+  it("should return List databases", async function () {
+    const result = await qiu.exec("\\l", { value: true });
+    expect(result).to.be.include("List of databases");
+  });
 
-    const checkTableQuery = `SHOW TABLES;`;
+  it("should load SQL Query from Files", async function () {
+    const createTableQuery = path.join(__dirname, "sql", "user_table.sql");
+    const start = Date.now();
+    await qiu.exec(createTableQuery, { isFile: true });
+    const checkTableQuery = `
+    SELECT table_name 
+    FROM information_schema.tables 
+    WHERE table_schema = 'public' AND table_name = 'user';
+  `;
+
+    // Log result and ensure result is defined
     const result = await qiu.exec(checkTableQuery, { value: true });
-    expect(result).to.include(tableName);
+    expect(result).to.include("user");
   });
 });
