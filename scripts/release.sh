4d95b2222436ed6143fee8102509af5e2e93ca1c
mahdi <mahdimamashli1383@gmail.com>
2024-09-28 03:07:01 +0330

    chore: The package release script has been updated and the content of the release tag is displayed on GitHub.

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
-
 npm run build
 VERSION=$(node -p "require('./package.json').version")
-
-CHANGELOG_CONTENT=$(cat docs/CHANGELOG.md)
-git tag -a "v${VERSION}" -m "Release ${VERSION}: ${CHANGELOG_CONTENT}"
+CHANGELOG_CONTENT=$(awk "/## \\[$VERSION\\]/{flag=1;next}/^## /{flag=0}flag" docs/CHANGELOG.md)
+FORMATTED_CHANGELOG=$(echo "${CHANGELOG_CONTENT}" | sed -E 's/^### /#### /g; s/^### //g; s/^### /###\n### /g')
+OUTPUT="## [${VERSION}] - $(date +%Y-%m-%d)\n\n${FORMATTED_CHANGELOG}\n"
+echo -e "${FINAL_OUTPUT}"
 npm publish --access public
+git tag -a "v${VERSION}" -m "Release ${VERSION}: ${OUTPUT}"
 git add .
 git commit -m "chore: release version ${VERSION}"
 git push origin main --tags
+echo "Release ${VERSION} completed successfully!"
